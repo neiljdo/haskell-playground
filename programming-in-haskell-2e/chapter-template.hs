099785b174b304c51c97d2ce631bc456f660d132
Neil John D. Ortega <neiljohn.ortega+git@gmail.com>
2020-02-22 15:14:25 +0900

    Add chapter template

---
+++ programming-in-haskell-2e/chapter-template.hs
@@ -0,0 +1,27 @@
+-- 6. Title
+
+-- 6.1 Section
+
+-- 6.2 Section
+
+-- 6.3 Section
+
+-- 6.4 Section
+
+-- 6.5 Section
+
+-- 6.6 Section
+-- Details
+-- 1. 
+-- 2. 
+-- 3. 
+
+
+-- Exercises
+-- 1.
+-- 2.
+-- 3.
+
+
+main = do
+    print "Exercises"
