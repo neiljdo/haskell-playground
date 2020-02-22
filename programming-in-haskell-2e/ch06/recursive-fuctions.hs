1c7b87eeddf0ba2da3ed1ae53320b2aefad86bda
Neil John D. Ortega <neiljohn.ortega+git@gmail.com>
2020-02-22 15:15:00 +0900

    Add notes template for Chapter 6

---
+++ programming-in-haskell-2e/ch06/recursive-fuctions.hs
@@ -0,0 +1,30 @@
+-- 6. Recursive functions
+
+-- 6.1 Basic concepts
+
+-- 6.2 Recursion on lists
+
+-- 6.3 Multiple arguments
+
+-- 6.4 Multiple recursion
+
+-- 6.5 Mutual recursion
+
+-- 6.6 Advice on recursion
+-- Steps:
+-- 1. Define the type
+-- 2. Enumerate the cases
+-- 3. Define the simple cases
+-- 4. Define the other cases
+-- 5. Generalise and simplify
+
+
+-- Exercises
+-- 1.
+-- 2.
+-- 3.
+
+
+
+main = do
+    print "Exercises"
