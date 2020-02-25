766386e6e92d1d299ec6fd0349b4353740e2e58b
Neil John D. Ortega <neiljohn.ortega+git@gmail.com>
2020-02-25 18:08:45 +0900

    Write solutions to Chapter 6 exercises

--- programming-in-haskell-2e/ch06/recursive-functions.hs
+++ programming-in-haskell-2e/ch06/recursive-functions.hs
@@ -79,11 +79,53 @@ odd_ n = even_ (n - 1)
 --    from the argument during the recursive step. An improvement would be
 facR2 :: Int -> Int
 facR2 0 = 1                                          -- Base case
-facR2 n | n < 0          = -1                        -- Bad case, but need to return an Int output
-        | otherwise      = n * facR2 (n - 1)          -- (Proper) recursive case
+facR2 n | n < 0         = -1                         -- Bad case, but need to return an Int output
+        | otherwise     = n * facR2 (n - 1)          -- (Proper) recursive case
 
--- 2.
--- 3.
+-- 2. Define a recursive function `sumdown` that returns the sum of the non-negative
+--    integers from a given value down to zero. For example
+--    > sumdown 3
+--    6
+sumdown :: Int -> Int
+sumdown 0 = 0
+sumdown n | n < 0       = -1
+          | otherwise   = n + sumdown (n - 1)
+
+-- 3. Define the exponentiation operator `^` for non-negative integers using
+--    the same pattern of recursion as the multiplication operator `*`, and show how
+--    the expressions 2 ^ 3 is evaluated using your definition.
+--    > pow 2 3
+--      = 2 * pow 2 2
+--      = 2 * 2 * pow 2 1
+--      = 2 * 2 * 2 * pow 2 0
+--      = 2 * 2 * 2 * 1
+--    8
+pow :: Int -> Int -> Int
+pow _ 0 = 1
+pow a b = a * pow a (b - 1)
+
+-- 4. Define a recursive function `euclid` that implements _Euclid's algorithm_
+--    for calculating the greatest common divisor of two non-negative integers: if
+--    the two numbers are equal, this number is the result; otherwise, the smaller
+--    number is subtracted from the larger, and the same process is then repeated.
+--    For example:
+--    > euclid 6 27
+--    3
+euclid :: Int -> Int -> Int
+euclid m n | m == n         = m
+           | m > n          = euclid (m - n) n
+           | otherwise      = euclid m (n - m)
+
+-- 5. Using the recursive definitinos givin in this chapter, show how the following
+--    are evaluated:
+--    a) > length [1,2,3]
+--       = 1 + length [2,3]
+--       = 1 + 1 + length [3]
+--       = 1 + 1 + 1 + length []
+--       = 1 + 1 + 1 + 0
+--       3
+--    b) > drop 3 [1,2,3,4,5]
+--    c) > init [1,2,3]
 
 
 
@@ -94,4 +136,12 @@ main = do
     print (insert 3 [1,2,4,5])
     print (insertionSort [5,4,2,-1,0,-2,3])
     print (quickSort [5,4,2,-1,0,-2,3])
+
     print "Exercises"
+    print (facR2 (-1))
+    print (sumdown (3))
+    print (sumdown (-1))
+    print (pow 2 3)
+    print (euclid 6 27)
+    print (euclid 5 27)
+    print (euclid 18 27)
