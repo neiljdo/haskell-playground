04ab3ff9348ddf7fa44ef2d364334988ba5f5ed5
Neil John D. Ortega <neiljohn.ortega+git@gmail.com>
2020-02-29 03:21:39 +0900

    Add answers to challenges, chapter summary

--- category-theory-for-programmers/ch01/README.md
+++ category-theory-for-programmers/ch01/README.md
@@ -1,2 +1,26 @@
 # Category: The Essence of Composition
 
+## Definitions
+
+A **category** is a collection of *objects* and *arrows* (or *morphisms*) between objects.
+
+Morphisms **compose** if the end of one is the start of another.
+
+e.g. If objects `A`, `B`, and `C`, and morphisms `f := A -> B` and `g := B -> C` are in a category, then we must also have `g . f := A -> C`.
+
+## Properties of composition
+
+Composition obeys:
+
+-   *Associativity*
+    If `f := A -> B`, `g := B -> C`, `h := C -> D`, then:
+    `(h . g) . f = h . (g . f) = h . g . f`
+
+-   *Identity*
+    Each object `X` in a category has its identity morphism `id_X`.
+    If `f := A -> B`, then:
+    `f . id_A = f = id_B . f`.
+
+## A change of perspective
+
+In set theory, we can look at each individual elements of a set but in category theory, we zoom out far enough that we abstract entire sets into a single point.
--- category-theory-for-programmers/ch01/challenges.hs
+++ category-theory-for-programmers/ch01/challenges.hs
@@ -11,8 +11,40 @@ compose g f x = g (f x)
 --    My initial attempt for the compose function above was as follows:
 --    compose :: (b -> c) -> (a -> b) -> (a -> c)
 --    Why wouldn't this type work?
+--    EDIT: This **ACTUALLY** works! Here, the return function must be:
+--    a) a function;
+--    b) whose input type is a; and,
+--    c) whose output type is b
+compose2 :: (b -> c) -> (a -> b) -> (a -> c)
+compose2 g f = \x -> g (f x)
 
-dblThenInc1 = compose (\x -> x + 1) (\x -> 2 * x)
+dblThenInc1      = compose (\x -> x + 1) (\x -> 2 * x)
+dblThenInc1Again = compose (\x -> x + 1) (\x -> 2 * x)
+
+-- 3. Write a program that tries to test that your composition function respects identity.
+--    TODO: Missing answer.
+
+-- 4. Is the world-wide web a category in any sense? Are links morphisms?
+--    Answer: If web pages are the objects with links as morphisms, then it is not a category.
+
+-- 5. Is Facebook a category, with people as objects and friendships as morphisms?
+--    Answer: No. The "friendship morphism" is not composable, i.e. the friend of
+--    my friend is not necessarily my friend.
+
+-- 6. When is a directed graph a category?
+--    Answer: Given any directed graph, we can turn it into a category by adding:
+--    a) arrows from a node to itself (identity); and,
+--    b) arrows connecting the endpoints of any length-2 path (composition), like so:
+
+{-
+NOTE: Identity arrows not drawn for brevity.
+                                    ___________
+                                   /           v             
+      o ---> o ---> o             o ---> o ---> o
+              \            =>      \     \
+               \---> o              \     \---> o
+                                     \---------^
+-}
 
 
 main = do
@@ -21,4 +53,5 @@ main = do
 
     print (((\x -> x + 1) . (\x -> 2 * x)) 3)
     print (dblThenInc1 3)
+    print (dblThenInc1Again 3)
     
