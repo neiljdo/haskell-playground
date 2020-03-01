74f7dd595e096a58df381d76ba0af1beb55a2556
Neil John D. Ortega <neiljohn.ortega+git@gmail.com>
2020-03-01 17:03:43 +0900

    Add PiH2 Chapter 8 note template

---
+++ programming-in-haskell-2e/ch08/declaring-types-and-classes.hs
@@ -0,0 +1,30 @@
+-- 8. Declaring types and classes
+
+-- 8.1 Type declarations
+
+-- 8.2 Data declarations
+-- - How is it different from other
+
+-- 8.3 Newtype declarations
+-- - How is it different from other
+
+-- 8.4 Recursive types
+
+-- 8.5 Class and instance declarations
+-- - Similar to class and instances in OOP languages?
+
+-- 8.5.1 Derived instances
+
+-- 8.6 Tautology checker
+
+-- 8.7 Abstract machine
+
+
+-- Exercises
+-- 1.
+-- 2.
+-- 3.
+
+
+main = do
+    print "Exercises"
