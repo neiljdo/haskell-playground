e561ab52e0e091d54e9c36fb5800b21a8064c5b5
Neil John D. Ortega <neiljohn.ortega+git@gmail.com>
2020-03-05 21:58:40 +0900

    Add notes for PiH2 Chapter 8

--- programming-in-haskell-2e/ch08/declaring-types-and-classes.hs
+++ programming-in-haskell-2e/ch08/declaring-types-and-classes.hs
@@ -1,8 +1,43 @@
 -- 8. Declaring types and classes
 
 -- 8.1 Type declarations
+-- NOTE: We capitalize the first letter of the type name for "concrete" types
+type String_ = [Char]
+
+-- * Types can be nested, e.g.
+type Pos = (Int, Int)
+type Trans = Pos -> Pos
+
+-- * Types CANNOT BE RECURSIVE, e.g.
+-- type Tree = (Int, [Tree])        # this will throw an exception
+
+-- * Types can be parametrized (one or more parameters), e.g.
+type Pair a = (a, a)                -- That is, a `Pair` is a tuple of *any* type
+type Assoc k v = [(k, v)]           -- `Assoc` is a kind of strict dictionary,
+                                    -- where all keys must be of the same type `k`,
+                                    -- and all values must be of the same type `v`
+
+
+-- With `Assoc` above, we can define a lookup function like so,
+-- Q: Is this O(n)?
+find :: Eq k => k -> Assoc k v -> v
+find k d = head [v | (k', v) <- d, k == k']
 
 -- 8.2 Data declarations
+-- Enumerates all possible values. In the example below,
+-- `False` and `True` are called *constructors*.
+data Bool_ = False | True
+
+-- * Constructor names must also begin with capital letters
+-- * Types cannot share constructor names
+
+-- !!! Names given to new types and constructors have no inherent meaning.
+-- The following is the same data declaration of `Bool_` above:
+data Foo = Bar | Baz
+-- How the type behaves is ultimately defined by the programmer
+
+
+
 -- - How is it different from other
 
 -- 8.3 Newtype declarations
@@ -27,4 +62,7 @@
 
 
 main = do
+    print "8.1 Type declarations"
+    let d = [('a', 1), ('b', 2), (3, 'c')]
+    print (find 'a' d)
     print "Exercises"
